eeeace1cfd192bd1acdc22ed274edead68f9d019
Erika Vazquez <vasquezerika15@yahoo.com>
2018-04-26 20:06:18 -0500

    Update MainIndex.js

--- MainIndex.js
+++ MainIndex.js
@@ -12,14 +12,49 @@ application.use(express.static(__dirname + "/public"));
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'imtheone2016',
   database: 'degPlanDB'
 });
 
 
 
+application.get("/", function (req, res) {
+var q = "SELECT deparment_name, department_id FROM DEPARTMENT";
+var r = "SELECT COURSE.course_id, COURSE.credit_hours, COURSE.TCCNS, COURSE_SUBJECT.COURSE_SUBJECT_id FROM COURSE INNER JOIN COURSE_SUBJECT ON COURSE.COURSE_SUBJECT_id = 14 ORDER BY COURSE_SUBJECT_id";
 
-//homepage
+  connection.connect(function (err) {
+    if (err) throw err;
+    connection.query(q, function (err, result) {
+      if (err) throw err;
+      result = JSON.stringify(result);
+      result = JSON.parse(result);
+      console.log(result);
+      res.render("majorReqmts", { data: result });
+      
+    }); 
+    
+    
+  });
+});
+
+
+function all(req, res, next){
+var q = "SELECT deparment_name FROM DEPARTMENT";
+  connection.query(q, function(err, row){
+    if(err) throw err;
+    req.department_id = row;
+    req.deparment_name = row;
+    req.course_id = row;
+    req.credit_hours = row;
+    req.TCCNS = row;
+    req.component_id = row;
+    return next();
+  });
+}
+
+
+
+/*
 application.get("/", function (req, res) {
 var q = "SELECT COURSE.course_id, COURSE.credit_hours, COURSE.TCCNS, COURSE_SUBJECT.COURSE_SUBJECT_id FROM COURSE INNER JOIN COURSE_SUBJECT ON COURSE.COURSE_SUBJECT_id = 14 ORDER BY COURSE_SUBJECT_id";
   connection.connect(function (err) {
@@ -36,12 +71,22 @@ var q = "SELECT COURSE.course_id, COURSE.credit_hours, COURSE.TCCNS, COURSE_SUBJ
 });
 
 
+function all(req, res, next){
+  var q = "SELECT course_id from COURSE ";
+  connection.query(q, function(err, row){
+    if(err) throw err;
+    req.course_id = row;
+    req.credit_hours = row;
+    req.TCCNS = row;
+    req.component_id = row;
+    return next();
+  });
+}
 
-
-
+*/
  
 application.get("*", function (req, res) {
-  res.send("Error");
+  res.send("Page not found");
 });
 
 application.listen(8000, function () {
